2cb5bed43fc425984f4a321108e48ec445aac7bb
Keidy Y Alcantara <kya5@afsconnect1.njit.edu>
2020-10-11 17:51:58 -0400

    hi

---
+++ project/partials/index.php
@@ -0,0 +1,4 @@
+
+<?php
+header("Location: /afs/cad.njit.edu/u/k/y/kya5/public_html/IT202/project/index.php");
+?>
---
+++ project/sql/index.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: /afs/cad.njit.edu/u/k/y/kya5/public_html/IT202/project/index.php");
+?>
