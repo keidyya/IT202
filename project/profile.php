05ab8b91c82095869705b1f2dd27d24b213c09f8
Keidy Y Alcantara <kya5@afsconnect1.njit.edu>
2020-10-12 19:25:10 -0400

    pls work

--- project/profile.php
+++ project/profile.php
@@ -1,5 +1,7 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<?php
+<?php 
+
+require_once(__DIR__ . "/partials/nav.php");
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
