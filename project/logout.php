f7216d5d183777df077390f578e0a8fc6b30a41e
Keidy Y Alcantara <kya5@afsconnect1.njit.edu>
2020-11-07 18:18:41 -0500

    sigh

--- project/logout.php
+++ project/logout.php
@@ -5,7 +5,9 @@ session_unset();
 // destroy the session
 session_destroy();
 ?>
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php require_once(__DIR__ . "/partials/nav.php");/*ultimately, this is just here for the function to be loaded now*/ ?>
 <?php
-echo "You're logged out (proof by dumping the session)<br>";
+
+flash("You have been logged out");
+die(header("Location: login.php"));
 ?>
