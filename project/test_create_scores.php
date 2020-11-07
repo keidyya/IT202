8cc80b7c6339ba2fb899ae2e65a8b1fd69c31cc7
Keidy Y Alcantara <kya5@afsconnect1.njit.edu>
2020-11-07 17:18:57 -0500

    lets try this again

---
+++ project/test_create_scores.php
@@ -0,0 +1,40 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+
+<form method="POST">
+	<label>Name</label>
+	<input name="name" placeholder="Name"/>
+	<label>Score</label>
+	<input type="number" min="10000000000000" name="score"/>
+	<input type="submit" name="save" value="Create"/>
+</form>
+
+<?php
+if(isset($_POST["save"])){
+	//TODO add proper validation/checks
+	$name = $_POST["id"];
+	$score = $_POST["score"];
+	$user = get_user_id();
+	$db = getDB();
+	$stmt = $db->prepare("INSERT INTO Scores (id, user_id, score) VALUES(:id, :user, :score)");
+	$r = $stmt->execute([
+		":id"=>$name,
+		":score"=>$score,
+		":user"=>$user
+	]);
+	if($r){
+		flash("Created successfully with id: " . $db->lastInsertId());
+	}
+	else{
+		$e = $stmt->errorInfo();
+		flash("Error creating: " . var_export($e, true));
+	}
+}
+?>
+<?php require(__DIR__ . "/partials/flash.php");
